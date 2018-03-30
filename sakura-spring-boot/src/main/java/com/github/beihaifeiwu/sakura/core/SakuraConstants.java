c49181405e8645f59d49c2bd364b7ba22fabb7d5
haomu <haomu@pinduoduo.com>
2018-03-30 14:52:02 +0800

    upgrade dependencies

--- sakura-spring-boot/src/main/java/com/github/beihaifeiwu/sakura/core/SakuraConstants.java
+++ sakura-spring-boot/src/main/java/com/github/beihaifeiwu/sakura/core/SakuraConstants.java
@@ -9,22 +9,15 @@ import lombok.experimental.UtilityClass;
 public class SakuraConstants {
 
     public static final String DEFAULT_SPRING_CONFIG_SOURCE = "classpath:application-sakura-default.properties";
-    public static final String DEFAULT_BEETL_CONFIG_SOURCE = "classpath:sakura-beetl-default.properties";
     public static final String DEFAULT_EXCEPTION_MESSAGE_SOURCE = "classpath*:/sakura-exception-mapping";
 
     public static final String SPRING_VELOCITY_MACROS = "spring-velocity-macros.vm";
 
     public static final String DEFAULT_PROP_PREFIX = "sakura";
-    public static final String BEETL_PROP_PREFIX = DEFAULT_PROP_PREFIX + ".beetl";
-    public static final String COMMAND_PROP_PREFIX = DEFAULT_PROP_PREFIX + ".command";
-    public static final String CURATOR_PROP_PREFIX = DEFAULT_PROP_PREFIX + ".curator";
     public static final String EXECUTOR_PROP_PREFIX = DEFAULT_PROP_PREFIX + ".executor";
-    public static final String FEIGN_PROP_PREFIX = DEFAULT_PROP_PREFIX + ".feign";
     public static final String OKHTTP_PROP_PREFIX = DEFAULT_PROP_PREFIX + ".okhttp";
     public static final String SCHEDULER_PROP_PREFIX = DEFAULT_PROP_PREFIX + ".scheduler";
-    public static final String SPRINGFOX_PROP_PREFIX = DEFAULT_PROP_PREFIX + ".springfox";
     public static final String VELOCITY_PROP_PREFIX = DEFAULT_PROP_PREFIX + ".velocity";
     public static final String WEB_PROP_PREFIX = DEFAULT_PROP_PREFIX + ".web";
-    public static final String ZOOKEEPER_PROP_PREFIX = DEFAULT_PROP_PREFIX + ".zookeeper";
 
 }

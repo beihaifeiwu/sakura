3c2297a33e2b03d4b43101cd3b3ffb018a74f898
haomu <haomu@pinduoduo.com>
2018-04-23 17:54:51 +0800

    add cron expression

---
+++ sakura-common/src/test/java/sakura/common/util/CronExpressionTest.java
@@ -0,0 +1,64 @@
+package sakura.common.util;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by haomu on 2018/4/23.
+ */
+public class CronExpressionTest {
+
+    @Test
+    public void testIsSatisfiedBy() throws Exception {
+        CronExpression cronExpression = new CronExpression("0 15 10 * * ? 2005");
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.set(2005, Calendar.JUNE, 1, 10, 15, 0);
+        assertTrue(cronExpression.isSatisfiedBy(cal.getTime()));
+
+        cal.set(Calendar.YEAR, 2006);
+        assertFalse(cronExpression.isSatisfiedBy(cal.getTime()));
+
+        cal = Calendar.getInstance();
+        cal.set(2005, Calendar.JUNE, 1, 10, 16, 0);
+        assertFalse(cronExpression.isSatisfiedBy(cal.getTime()));
+
+        cal = Calendar.getInstance();
+        cal.set(2005, Calendar.JUNE, 1, 10, 14, 0);
+        assertFalse(cronExpression.isSatisfiedBy(cal.getTime()));
+    }
+
+    @Test
+    public void testLastDayOffset() throws Exception {
+        CronExpression cronExpression = new CronExpression("0 15 10 L-2 * ? 2010");
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.set(2010, Calendar.OCTOBER, 29, 10, 15, 0); // last day - 2
+        assertTrue(cronExpression.isSatisfiedBy(cal.getTime()));
+
+        cal.set(2010, Calendar.OCTOBER, 28, 10, 15, 0);
+        assertFalse(cronExpression.isSatisfiedBy(cal.getTime()));
+
+        cronExpression = new CronExpression("0 15 10 L-5W * ? 2010");
+
+        cal.set(2010, Calendar.OCTOBER, 26, 10, 15, 0); // last day - 5
+        assertTrue(cronExpression.isSatisfiedBy(cal.getTime()));
+
+        cronExpression = new CronExpression("0 15 10 L-1 * ? 2010");
+
+        cal.set(2010, Calendar.OCTOBER, 30, 10, 15, 0); // last day - 1
+        assertTrue(cronExpression.isSatisfiedBy(cal.getTime()));
+
+        cronExpression = new CronExpression("0 15 10 L-1W * ? 2010");
+
+        cal.set(2010, Calendar.OCTOBER, 29, 10, 15, 0); // nearest weekday to last day - 1 (29th is a friday in 2010)
+        assertTrue(cronExpression.isSatisfiedBy(cal.getTime()));
+    }
+
+}
